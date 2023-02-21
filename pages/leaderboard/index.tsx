d7f65b70abfa61b973ac171ed43dea847b7797fa
christeenamjoy <christmjoy@gmail.com>
2023-02-21 19:43:21 +0900

    Base structure is added API integrated Show the data from API in both leaderboard and Profile

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,17 +1,43 @@
-import { FC } from "react"
+import Link from "next/link"
+import { FC, useEffect, useState } from "react"
 
 
 
 const Leaderboard: FC = () => {
 
+  const [leaderBoard,setLeaderBoard] =useState<UserDetails[]>([])
 
-  return (<>
-    <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
-      <h1 className="text-4xl font-bold">Leaderboard</h1>
-      <p>TODO: complete.</p>
-      <p>All leaderboard entries should be links to user profile page.</p>
-    </div>
-  </>)
+  useEffect(()=>{
+const interval = setInterval(()=>{
+  getData()
+},20000)
+getData()
+
+
+return () =>{
+  clearInterval(interval)
+}
+  },[])
+
+  const getData = async () =>{
+         const response = await fetch("/api/leaderboard")
+         const data = await response.json()
+         setLeaderBoard(data.leaderboard)
+
+  }
+
+  return (<div className="flex flex-wrap m-10">
+
+  {leaderBoard?.map((player) => {
+    return (          <Link href={`/profile/${encodeURIComponent(player.username)}`} key={player.username}>
+    <div  className="max-w-lg text-black w-40 border border-black m-2 p-2 text-center justify-center grid">
+            <img src={player.profileImage} alt="profile-image" className="h-20 rounded-full"/>
+      <div >{player.username}</div>
+      <div >{player.score}</div>
+    </div></Link>)
+  })}
+ 
+  </div>)
 
 }
 
--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -1,14 +1,32 @@
-import { FC } from "react"
-
+import { FC, useEffect, useState } from "react"
+import { useRouter } from 'next/router'
 
 
 const User: FC = () => {
+  const [data,setData] = useState<ProfileData>()
 
+  const router = useRouter()
+  const { username } = router.query
+  console.log(username)
+
+  useEffect(()=>{
+    const getPlayerData = async () =>{
+      const response = await fetch(`/api/profile/${username}`)
+      const data = await response.json()
+      setData(data)
+      
+        }
+    getPlayerData()
+  },[username])
 
   return (<>
-    <div className="w-full h-80 flex flex-col items-center justify-center space-y-12">
-      <h1 className="text-4xl font-bold">User</h1>
-      <p>TODO: complete</p>
+    <div className="w-full  text-black">
+      <h1 className="text-4xl font-bold m-2">Username : {data?.username}</h1>
+      <h1 className="text-2xl m-2">Bio : {data?.bio}</h1>
+      <h1 className="text-2xl m-2">Age: {data?.age}</h1>
+      <h1 className="text-2xl m-2">Twitter :{data?.twitter}</h1>
+      <h1 className="text-2xl m-2">Email :{data?.email}</h1>
+
     </div>
   </>)
 
